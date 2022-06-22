9a4346ce04ed4c982988d6ac2e42ba6acc9449b8
Nguyễn Cao Khôi <93377917+caokhoi2702@users.noreply.github.com>
2022-06-23 01:09:27 +0700

    server&client demo#1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string clientIP = "127.0.0.1";
+            const int clientPort = 6969;
+
+        connection:
+            try
+            {
+                TcpClient client = new TcpClient(clientIP, clientPort);
+                string message = "Hello";
+
+                int byteCount = Encoding.ASCII.GetByteCount(message + 1);
+                byte[] sendData = new byte[byteCount];
+                sendData = Encoding.ASCII.GetBytes(message);
+
+                NetworkStream stream = client.GetStream();
+                stream.Write(sendData, 0, sendData.Length);
+
+                StreamReader sr = new StreamReader(stream);
+                string response = sr.ReadLine();
+                Console.WriteLine("Server: " + response);
+
+                stream.Close();
+                client.Close();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to connect.");
+                goto connection;
+            }
+        }
+    }
+}
