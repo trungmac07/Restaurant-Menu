232d8b15dbc3ed580b5c775ae020b27ca0344ea2
Ha0203 <mr.stsh020303@gmail.com>
2022-06-24 19:11:27 +0700

    Xong class r nhaaa

---
+++ CLIENT/Client/Client/Client/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        private
+        TcpClient client;
+        NetworkStream stream;
+        StreamReader sr;
+        StreamWriter sw;
+        public
+
+        Program()
+        {
+            client = new TcpClient("127.0.0.1", 6969);
+            stream = client.GetStream();
+            sr = new StreamReader(stream);
+            sw = new StreamWriter(stream);
+        }
+
+        ~Program() 
+        {
+            stream.Close();
+            client.Close();
+            Console.ReadKey();
+        }
+
+        void recvString()
+        {
+            string str;
+            while (true)
+            {
+                str = sr.ReadLine();
+                if(str != "")
+                    Console.WriteLine(str);
+            } 
+        }
+
+        void recvByte()
+        {
+            byte[] buffer = new byte[69000];
+            stream.Read(buffer, 0, buffer.Length);
+            int recv = 0;
+            foreach (byte b in buffer)
+            {
+                if (b != 0)
+                {
+                    recv++;
+                }
+            }
+            string msg_recv = Encoding.UTF8.GetString(buffer, 0, recv);
+            Console.WriteLine(msg_recv);
+        }
+        void sendString()
+        {
+            string str = Console.ReadLine();
+            sw.WriteLine(str);
+            sw.Flush();
+        }
+        static void Main(string[] args)
+        {
+        connection:
+            try
+            {
+                Program a = new Program();
+                //Gửi tin nhắn cho bên server
+                a.sendString();
+
+                //Đọc tin nhắn từ bên server gửi qua
+                a.recvString();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed to connect...");
+                goto connection;
+            }
+        }
+    }
+}
+
---
+++ CLIENT/Client/Server/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 6969);
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection.");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client accepted.");
+                NetworkStream stream = client.GetStream();
+                StreamReader sr = new StreamReader(stream);
+                StreamWriter sw = new StreamWriter(stream);
+                try
+                {
+                    string response = sr.ReadLine();
+                    Console.WriteLine(response);
+                    sw.WriteLine("Hi ban");
+                    sw.Flush();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong.");
+                    sw.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
